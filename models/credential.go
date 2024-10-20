5a7679c005b439a90e8656a4a4ad38b27cf94a77
Laysa <laysa.developer@gmail.com>
2024-10-20 15:19:20 -0300

    feat: add id to db model

--- models/credential.go
+++ models/credential.go
@@ -1,8 +1,12 @@
 package models
 
-import "time"
+import (
+    "go.mongodb.org/mongo-driver/bson/primitive"
+    "time"
+)
 
 type Credential struct {
+    ID          primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
     ServiceName string    `json:"service-name"`
     User        string    `json:"user"`
     Password    string    `json:"password"`
